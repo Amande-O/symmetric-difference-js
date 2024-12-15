2d6f4783f127902ea5812d591ac549ffb14a47bb
Amande-O <oliger.amandine@gmail.com>
2024-12-15 19:38:11 +0100

    add new sets and limit the number of additions

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const submitBtn = document.querySelector('#submit');
 const mainSection = document.querySelector('main');
+const btnAdd = document.querySelector('#btn-Add');
+const form = document.querySelector('form');
+const sectionBtn = document.querySelector('.section__btn');
+let setName;
 
+btnAdd.addEventListener('click', addNewSet);
 submitBtn.addEventListener('click', showSymDiff);
 
 // shows the symmetric difference
@@ -15,9 +20,9 @@ function showDivResult(resultSymDiff){
 	let divResult = document.querySelector('.section__result');
 	let pResult = document.querySelector('#result');
 	if (pResult == undefined) {
-		divResult = document.createElement("section");
-		let firstP = document.createElement("p");
-		pResult = document.createElement("p");
+		divResult = document.createElement('section');
+		let firstP = document.createElement('p');
+		pResult = document.createElement('p');
 		divResult.classList.add('section__result');
 		pResult.id = "result";
 		mainSection.appendChild(divResult);
@@ -32,4 +37,43 @@ function showDivResult(resultSymDiff){
 function symDiff() {
 	let resultTest = ("1,5,9");
 	return resultTest
+}
+
+// adds a new set
+function addNewSet(e){
+	e.preventDefault();
+	if(!limitInputAddSet()){
+		let newLabel = document.createElement('label');
+		let newInput = document.createElement('input');
+		newInput.setAttribute('placeholder', `Saisir ici l'ensemble ${setName}`);
+		newInput.classList.add('input__text');
+		let contentLabel = document.createTextNode(`Saisie de l'ensemble ${setName}: `);
+		newLabel.appendChild(contentLabel);
+		form.insertBefore(newLabel, sectionBtn);
+		form.insertBefore(newInput, sectionBtn);
+	}
+}
+
+// checks the number of sets and adds a name to the set
+function limitInputAddSet() {
+	const nbInputText = document.querySelectorAll('.input__text').length;
+	let messageLimitAddSet = document.querySelector('.message__limit');
+	if(nbInputText == 5) {
+		if (messageLimitAddSet == null) {
+			messageLimitAddSet = document.createElement('p');
+			messageLimitAddSet.classList.add('message__limit');
+			messageLimitAddSet.textContent = "Ajout supplémentaire impossible. Dans cet exemple, le nombre d'ensemble est limité à 5.";
+			form.insertBefore(messageLimitAddSet, sectionBtn);
+		}
+		return true;
+	} else {
+		addSetName(nbInputText);
+		return false;
+	}
+}
+
+// adds the name of set
+function addSetName(nbInputText) {
+	let names = ["C", "D", "E"];
+	setName = (names[nbInputText - 2]);
 }
